9f3982bb2c330706fe36d94efbff55ca3c87f924
mcclellangg <81567927+mcclellangg@users.noreply.github.com>
2022-04-07 14:04:07 -0400

    added detailed tracker component

---
+++ components/DetailedTracker.js
@@ -0,0 +1,63 @@
+// Detailed Stat Tracker component
+import { useState } from "react";
+import styles from "../styles/Trackers.module.css";
+
+const DetailedTracker = ({ label, maxValue }) => {
+    const [currentTotal, setCurrentTotal] = useState(maxValue);
+    const [adjusterValue, setAdjusterValue] = useState("");
+    const [clicked, setClicked] = useState(false);
+
+    const handleAdd = () => setCurrentTotal(+currentTotal + +adjusterValue);
+    const handleDeduct = () => setCurrentTotal(+currentTotal - +adjusterValue);
+
+    return (
+        <>
+            <div className={styles.trackerContainer}>
+                <div className={styles.trackerContent}>
+                    <span className={styles.contentLabel}>{label}</span>
+                    <div className={styles.contentGroup}>
+                        {clicked ? (
+                            <input className={styles.contentInput}
+                                type="number"
+                                value={currentTotal}
+                                onMouseLeave={() => setClicked(!clicked)}
+                                onChange={(e) => setCurrentTotal(e.target.value)}>
+                            </input>
+                        ) :
+                            (
+                                <span className={styles.contentInputText}
+                                    onClick={() => setClicked(!clicked)}>{currentTotal}
+                                </span>
+                            )
+                        }
+                        <span className={styles.contentSep}> / </span>
+                        <span className={styles.contentText}>{maxValue}</span>
+                    </div>
+                </div>
+
+                <div className={styles.trackerContent}>
+                    <div className={styles.trackerAdjuster}>
+                        <button className={`${styles.adjusterBtn} ${styles.complement}`}
+                            onClick={handleAdd}>
+                            Add
+                        </button>
+                        <input className={styles.contentInput}
+                            type="number"
+                            min="0"
+                            max="999"
+                            value={adjusterValue}
+                            onChange={(e) => setAdjusterValue(e.target.value)}>
+                        </input>
+                        <button className={`${styles.adjusterBtn} ${styles.primary}`}
+                            onClick={handleDeduct}>
+                            Deduct
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <hr></hr>
+        </>
+    )
+};
+
+export default DetailedTracker;
---
+++ pages/Trackers.js
@@ -0,0 +1,112 @@
+// Trackers.js
+import DetailedTracker from "../components/DetailedTracker"
+import StatDiv from "../components/StatDiv";
+import styles from "../styles/Trackers.module.css"
+import { useState } from "react";
+
+const Trackers = () => {
+    const [newTracker, setNewTracker] = useState({
+        label: "",
+        maxValue: "",
+        trackerType: ""
+    });
+    const [trackers, setTrackers] = useState([
+        {
+            label: "HP",
+            maxValue: 35,
+            trackerType: "detail"
+        },
+        {
+            label: "Mana",
+            maxValue: 15,
+            trackerType: "detail"
+        },
+        {
+            label: "Spell Slots",
+            maxValue: 8,
+            trackerType: "brief"
+        },
+        {
+            label: "Channel Divinity",
+            maxValue: 15,
+            trackerType: "brief"
+        }
+    ]);
+    const updateNewTracker = (e) => {
+        const target = e.target;
+        const name = target.name;
+        const value = target.value;
+        setNewTracker({
+            ...newTracker,
+            [name]: value
+        });
+    }
+
+    const addTracker = (e) => {
+        e.preventDefault();
+        console.log("Tracker added!", newTracker);
+        setTrackers([...trackers, newTracker]);
+        setNewTracker({ label: "", maxValue: "", trackerType: "" })
+    };
+
+    return (
+        <>
+            <div className={styles.mainContainer}>
+                <h1>Trackers</h1>
+                {trackers.filter(t => t.trackerType === "detail").map((detailTracker, index) => (
+                    <DetailedTracker key={index.toString()} label={detailTracker.label} maxValue={detailTracker.maxValue} />
+                ))}
+                {trackers.filter(t => t.trackerType === "brief").map((briefTracker, index) => (
+                    <StatDiv key={index.toString()} statAbv={briefTracker.label} statTotal={briefTracker.maxValue} />
+                ))}
+                <hr></hr>
+                <span className={styles.formText}>Add Trackers :</span>
+                <form className={styles.newTrackerContainer} onSubmit={addTracker}>
+                    <input className={styles.trackerInput}
+                        placeholder="Label"
+                        name="label"
+                        value={newTracker.label}
+                        onChange={updateNewTracker}
+                        size={6}>
+                    </input>
+                    <input className={styles.trackerInput}
+                        placeholder="Max Value"
+                        name="maxValue"
+                        value={newTracker.maxValue}
+                        onChange={updateNewTracker}
+                        type="number"
+                        min="0"
+                        max="10000">
+                    </input>
+                    <div className={styles.columnContainer}>
+                        <span className={styles.smallLabel}>Select Type: </span>
+                        <div className={styles.wrapper}>
+                            <div className={styles.customInput}>
+                                <input type="radio"
+                                    id="detail"
+                                    name="trackerType"
+                                    value="detail"
+                                    onChange={updateNewTracker} />
+                                <label htmlFor="detail">Detail</label>
+                            </div>
+                            <div className={styles.customInput}>
+                                <input type="radio"
+                                    id="brief"
+                                    name="trackerType"
+                                    value="brief"
+                                    defaultChecked
+                                    onChange={updateNewTracker} />
+                                <label htmlFor="brief">Brief</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.container}>
+                        <button className={styles.addBtn} type="submit">+</button>
+                    </div>
+                </form>
+            </div>
+        </>
+    )
+};
+
+export default Trackers;
