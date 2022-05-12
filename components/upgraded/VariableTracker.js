81e7e889040693796f94d947c10014b1d26ab0f8
mcclellangg <81567927+mcclellangg@users.noreply.github.com>
2022-05-12 10:27:48 -0400

    created component for variable tracker

---
+++ components/upgraded/VariableTracker.js
@@ -0,0 +1,26 @@
+// Variable Tracker
+import styles from "./VariableTracker.module.css";
+
+const VariableTracker = () => {
+    return (
+        <>
+            <div className={styles.variableTracker}>
+                <div className={styles.contentWrapper}>
+                    <label>SPELL SLOTS</label>
+                    <div className={styles.valueWrapper}>
+                        <div>1</div>
+                        <div>/</div>
+                        <div>5</div>
+                    </div>
+                </div>
+                <label className={styles.variableAdj}>09</label>
+                <div className={styles.flexColumn}>
+                    <button>&#65291;</button>
+                    <button>&#8722;</button>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default VariableTracker;
--- pages/Log.js
+++ pages/Log.js
@@ -1,6 +1,7 @@
 // Updated Redesign of Log
 import styles from "../styles/Redesign.module.css"
 import TrackerForm from "../components/upgraded/TrackerForm";
+import VariableTracker from "../components/upgraded/VariableTracker";
 
 const Log = () => {
     return (
@@ -8,6 +9,21 @@ const Log = () => {
             <h1 className={styles.header}>THE ADVENTURE LOG</h1>
             <div className={styles.layoutContainer}>
                 <TrackerForm></TrackerForm>
+                <div className={styles.trackerContainer}>
+                    <VariableTracker></VariableTracker>
+                    <VariableTracker></VariableTracker>
+                    <hr></hr>
+                    <div className={styles.singleTracker}>
+                        <label>CHANNEL DIVINITY</label>
+                        <div className={styles.valueWrapper}>
+                            <div>25</div>
+                            <div>/</div>
+                            <div>30</div>
+                        </div>
+                        <button>&#8722;</button>
+                        <button>&#65291;</button>
+                    </div>
+                </div>
             </div>
         </>
     )
