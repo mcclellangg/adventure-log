98312559110cbc0ad6658ad06a129ce078b9c531
mcclellangg <81567927+mcclellangg@users.noreply.github.com>
2022-05-12 10:49:49 -0400

    moved single tracker to component

---
+++ components/upgraded/SingleTracker.js
@@ -0,0 +1,21 @@
+// Single Tracker
+import styles from "./SingleTracker.module.css";
+
+const SingleTracker = () => {
+    return (
+        <>
+            <div className={styles.singleTracker}>
+                <label>DEXTERITY</label>
+                <div className={styles.valueWrapper}>
+                    <div>1</div>
+                    <div>/</div>
+                    <div>20</div>
+                </div>
+                <button>&#8722;</button>
+                <button>&#65291;</button>
+            </div>
+        </>
+    );
+};
+
+export default SingleTracker;
--- pages/Log.js
+++ pages/Log.js
@@ -2,6 +2,7 @@
 import styles from "../styles/Redesign.module.css"
 import TrackerForm from "../components/upgraded/TrackerForm";
 import VariableTracker from "../components/upgraded/VariableTracker";
+import SingleTracker from "../components/upgraded/SingleTracker";
 
 const Log = () => {
     return (
@@ -12,17 +13,11 @@ const Log = () => {
                 <div className={styles.trackerContainer}>
                     <VariableTracker></VariableTracker>
                     <VariableTracker></VariableTracker>
+
                     <hr></hr>
-                    <div className={styles.singleTracker}>
-                        <label>CHANNEL DIVINITY</label>
-                        <div className={styles.valueWrapper}>
-                            <div>25</div>
-                            <div>/</div>
-                            <div>30</div>
-                        </div>
-                        <button>&#8722;</button>
-                        <button>&#65291;</button>
-                    </div>
+
+                    <SingleTracker></SingleTracker>
+                    <SingleTracker></SingleTracker>
                 </div>
             </div>
         </>
